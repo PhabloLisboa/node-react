84bbeef8a547f42e50f9456f1ccf043810848618
Phablo Lisboa <phablolisboa01@gmail.com>
2020-02-11 18:48:33 -0300

    Update User

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -5,10 +5,39 @@ class UserController {
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
-      return resp.status(404).json({ error: 'User already exists' });
+      return resp.status(400).json({ error: 'User already exists' });
     }
 
     const { id, name, email, provider } = await User.create(req.body);
+
+    return resp.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  async update(req, resp) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email: req.body.email },
+      });
+
+      if (userExists) {
+        return resp.status(400).json({ error: 'Email already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return resp.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
 
     return resp.json({
       id,
