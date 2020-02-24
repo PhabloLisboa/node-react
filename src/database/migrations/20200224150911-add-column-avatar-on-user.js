a6e46ea76d683c8f77f7dd18ad0d06286b341de0
Phablo Lisboa <phablolisboa01@gmail.com>
2020-02-24 12:45:40 -0300

    Upload de arquivos - Avatar  do usuario

---
+++ src/database/migrations/20200224150911-add-column-avatar-on-user.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+
+module.exports = {
+  up: queryInterface => {
+    return queryInterface.addColumn('users', 'avatar_id', {
+      type: Sequelize.INTEGER,
+      references: { model: 'files', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.removeColumn('users', 'avatar_id');
+  },
+};
